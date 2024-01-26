29fa65abd8295ff7c16f327a15dc2267073da55b
Murari072001 <murari.t2001@gmail.com>
2024-01-26 15:50:03 +0530

    map with edit

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,25 +1,39 @@
 import { connect } from "react-redux";
 import React from "react";
-import { func } from "prop-types";
 
 function Todolist(props) {
     let inp = React.useRef()
     const handleAdd = () => {
         props.add()
-        props.update(inp.current.value = "")
+        props.updatenewtodo(inp.current.value = "")
+    }
+    const handleEdit=(todo,i)=>{
+        props.toggleupdate(i)
+        props.updatenewtodo(inp.current.value = todo.task)
+    }
+    const handleUpdate=()=>{
+        props.update()
+        props.toggleupdate(null)
+        props.updatenewtodo(inp.current.value = "")
     }
     console.log(props)
     return (<div className="container border border-3 border-dark">
         <h1 className="m-3 p-3 border border-3 border-dark">TodoList</h1>
         <div className="m-3 p-3 border border-3 border-dark">
-            <input type="text" ref={inp} onKeyUp={(e) => { props.update(e.target.value) }} />
-            <button onClick={() => { handleAdd() }}>Add</button>
+            <div className="form-floating">
+            <input type="text" className="form-control" id="input" placeholder="Todo" ref={inp} onKeyUp={(e) => { props.updatenewtodo(e.target.value) }} />
+            <label for="input">Enter New Todo</label>
+            </div>
+             <br/>
+            {!props.isupdate&&<button className="btn w-100 btn-warning " onClick={() => { handleAdd() }}>Add</button>}
+            {props.isupdate&&<button className="btn w-100 btn-warning " onClick={() => { handleUpdate() }}>Update</button>}
             <ul className="mt-3">
                 {props.todos.map((todo, i) => {
                     return (
-                        <li className="m-2 p-2" style={todo.isdone ? { color: 'red', textDecoration: "line-through",backgroundColor:"green" } : { color: 'black' }} >{todo.task}
+                        <li className="m-2 p-2" style={todo.isdone ? { color: 'red', textDecoration: "line-through" } : { color: 'black' }} >{todo.task}
                             {!todo.isdone && <button className="btn btn-success ms-2" onClick={() => { props.done(i) }}>Done</button>}
                             {todo.isdone && <button className="btn btn-warning ms-2" onClick={() => { props.undo(i) }}>Undo</button>}
+                            <button className="btn btn-danger ms-2" onClick={() => {handleEdit(todo,i)}}>Edit</button>
                             <button className="btn btn-danger ms-2" onClick={() => { props.delete(i) }}>Delete</button>
                         </li>)
                 })}
@@ -33,11 +47,14 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        update: (value) => { dispatch({ type: 'updatenewtodo', payload: value }) },
+        updatenewtodo: (value) => { dispatch({ type: 'updatenewtodo', payload: value }) },
         add: () => { dispatch({ type: 'addtodo' }) },
         done: (i) => { dispatch({ type: 'donetodo', index: i }) },
         undo: (i) => { dispatch({ type: 'undotodo', index: i }) },
-        delete: (i) => { dispatch({ type: 'delete', index: i }) }
+        delete: (i) => { dispatch({ type: 'delete', index: i }) },
+        edit:()=>{dispatch({type:"edit"})},
+        toggleupdate:(i)=>{dispatch({type:'toggleupdate',index:i})},
+        update:()=>{dispatch({type:'update'})}
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Todolist)
--- src/store/reducers/TodolistReducer.jsx
+++ src/store/reducers/TodolistReducer.jsx
@@ -4,16 +4,21 @@ const initialValue={
     newtodo:{
         task:'',
         isdone:false,
-    }
+    },
+    isupdate:false,
+    updateindex:false
+
 }
 
 let TodoReducer=(state=initialValue,action)=>{
     if(action.type==='updatenewtodo')
     {
+        console.log(action);
         return {...state,newtodo:{task:action.payload,isdone:false}}
     }
     if(action.type==='addtodo')
     {
+        if(state.newtodo.task)
         return {...state,todos:[...state.todos,state.newtodo]}
     }
     if(action.type === 'delete'){
@@ -30,6 +35,17 @@ let TodoReducer=(state=initialValue,action)=>{
         var temp=[...state.todos]
         temp[action.index].isdone=false
         return {...state,todos:[...temp]}
+    }
+    if(action.type==='toggleupdate')
+    {
+        return {...state,isupdate:!state.isupdate,updateindex:action.index}
+    }
+    if(action.type==='update')
+    {
+        if(state.todos.length!==0)
+        {var temp=[...state.todos]
+        temp[state.updateindex].task=state.newtodo.task
+        return {...state,todos:[...temp]}}
     }
     return state
 }
