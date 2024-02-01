2f2815fe6a922ad8c76bc83840d3ae24530272cc
Murari072001 <murari.t2001@gmail.com>
2024-02-01 15:35:37 +0530

    completed

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,23 +2,28 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 
-function UserDashBoard(props)
-{
-    const navi=useNavigate()
-    console.log("dash",props);
+function UserDashBoard(props) {
+    const navi = useNavigate()
+    console.log("dash", props);
     return (
-       <div>
-         <h1>Welcome:{props.login.gender==='male'?'Mr.':'Mrs.'}{props.login.firstname}</h1>
-         <Outlet></Outlet>
-       </div>
+        <>
+        {
+            props.allproducts.length===0 &&
+            <div className="text-center"><img src="https://i.pinimg.com/originals/c7/e1/b7/c7e1b7b5753737039e1bdbda578132b8.gif"/></div>
+        }
+            {props.allproducts.length > 0 && <div>
+                <h1>Welcome:{props.login.gender === 'male' ? 'Mr.' : 'Mrs.'}{props.login.firstname}</h1>
+                <Outlet></Outlet>
+            </div>}
+        </>
     )
 }
-const mapStateToProps=(state)=>{
+const mapStateToProps = (state) => {
     return state.login
 }
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return {
-        inc:()=>{dispatch({})}
+        inc: () => { dispatch({}) }
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(UserDashBoard)
+export default connect(mapStateToProps, mapDispatchToProps)(UserDashBoard)
---
+++ src/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="bg-dark d-flex">
+
+            <div className="text-white"><h1>The Best Shopping Experience is Here</h1></div>
+            <div className="text-white">
+                <h4>Our Contacts</h4>
+                <p>Email:<a href="mailto:info.edupoly@gmail.com">info.edupoly@gmail.com</a></p>
+                <p>Customer Care: <a href="">Customercare.eduoly.com</a></p>
+
+            </div>
+            <div className="text-white">
+                <p>+91 9561214818</p>
+                <p>Address:372, HMT Hills Rd, Addagutta Society, Jal Vayu Vihar, Kukatpally, Hyderabad, Telangana 500085
+                    (9:AM to 8:PM IST)</p>
+            </div>
+            <div className="text-white">
+                <h4>Follow Us On</h4>
+                <button className="btn btn-dark bi bi-whatsapp"></button>
+                <button className="btn btn-dark bi bi-instagram"></button>
+                <button className="btn btn-dark bi bi-telegram"></button>
+                <button className="btn btn-dark bi bi-twitter"></button>
+                <button className="btn btn-dark bi bi-youtube"></button>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
