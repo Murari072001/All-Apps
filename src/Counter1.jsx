61c870c31cd61afdbda7838d51043cb0b83161d1
Murari072001 <murari.t2001@gmail.com>
2024-02-03 12:11:54 +0530

    asd

--- src/Counter1.jsx
+++ src/Counter1.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Counter1(props)
 {
     console.log(props);
-    return (<div className="border border-dark border-3 m-3 p-3">
+    return (<div className="counter border border-dark border-3 m-3 p-3">
         <h1>Counter 1:{props.counter1.count}</h1>
         <button className="btn btn-secondary m-2" onClick={()=>{props.dispatch({type:"Inc1"})}}>Inc</button>
         <button className="btn btn-secondary m-2" onClick={()=>{props.dispatch({type:"Dec1"})}}>Dec</button>
